e107b90101bfb4b009e4e7a31acbe11c4af747ce
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 11:30:42 +0300

    created config classes

--- config.py
+++ config.py
@@ -5,3 +5,22 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
     SECRET_KEY =os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    
+    DEBUG = True
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/trial'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
