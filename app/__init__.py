b29b5732fbd810fbc965fa21d04c6cceb23130ca
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 11:23:40 +0300

    initialized app

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db=SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
