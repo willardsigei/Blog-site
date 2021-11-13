61c0a0f15ff8b47613bddfc2854a1cd6b358a67e
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 13:01:16 +0300

    added users db and decorators

--- app/models.py
+++ app/models.py
@@ -14,3 +14,37 @@ class Quote:
         self.id =id
         self.author = author
         self.quote = quote
+
+    
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255)) 
+    blog = db.relationship('Blog', backref='user',passive_deletes=True,lazy="dynamic")   
+    comment = db.relationship('Comment', backref='user',passive_deletes=True,lazy="dynamic")
+    
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
