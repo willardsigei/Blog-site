89b50201ad9c7a7c918cf5c10211f4eeb9aed2e0
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 12:57:46 +0300

    registered bleuprints

--- app/__init__.py
+++ app/__init__.py
@@ -37,3 +37,22 @@ def create_app(config_name):
 
     # configure UploadSet
     configure_uploads(app,photos)
+
+
+
+    #Registering the blueprint
+
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    configure_request(app)
+
+
+
+    return app
