fd9f1ec0b2f121d5246c95c5b12b1df4258242e1
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 11:29:04 +0300

    added quotes api url

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
     SECRET_KEY =os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
