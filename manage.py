bd43bd9863f81c653eadc31635ba56d4c8dd9504
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 11:32:14 +0300

    instanciated app

--- manage.py
+++ manage.py
@@ -3,3 +3,19 @@ from app.models import User
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
+# Creating app instance
+
+app = create_app('development')
+pass
+
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
