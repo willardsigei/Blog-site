60e69031b0afe092e04a463caa471a878a0d7988
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-11-13 12:57:08 +0300

    app configs

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,26 @@ bootstrap = Bootstrap()
 db=SQLAlchemy()
 mail = Mail()
 photos = UploadSet('photos',IMAGES)
+
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    # Will add the views and forms
+
+
+    # configure UploadSet
+    configure_uploads(app,photos)
